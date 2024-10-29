3760663f5d224b96ac3e3b4744643493edc33063
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-29 15:59:05 -0400

    params added to axios request body for testing

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -2,40 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./ProductList.scss";
 const ProductList = () => {
-//   const baseURL = process.env.REACT_API_URL;
+  //   const baseURL = process.env.REACT_API_URL;
   const [productList, setProductList] = useState([]);
   useEffect(() => {
     axios
-      .get(`http://localhost:8080/products`, { productName: "phone" })
+      .get(`http://localhost:8080/products`, {
+        params: {
+          productName: "ramen",
+        },
+      })
       .then((respond) => {
         setProductList(respond.data);
-        console.log(respond.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
   if (productList.length) {
     return (
-      <div className="products">
-        {productList.map((item) => {
-          return (
-            <div key={item.position}>
-              <h1>{item.title}</h1>
-              <img src={item.thumbnail} alt={item.title} />
-              <h2>price: {item.price}</h2>
-              <h2>
-                {item.source}
+      <div>
+        <div className="products">
+          {productList.map((item) => {
+            return (
+              <div key={item.position} className="">
+                <h1>{item.title}</h1>
                 <img
-                  src={item.source_icon}
-                  alt="source-icon"
-                  className="source-icon"
+                  src={item.thumbnail}
+                  alt={item.title}
+                  className="product-image"
                 />
-              </h2>
-            </div>
-          );
-        })}
+                <h2>price: {item.price}</h2>
+                <h2>
+                  {item.source}
+                  <img
+                    src={item.source_icon}
+                    alt="source-icon"
+                    className="source-icon"
+                  />
+                </h2>
+              </div>
+            );
+          })}
+        </div>
       </div>
+    );
+  } else {
+    return (
+      <>
+        <di>
+          <h1>Loading.....</h1>
+        </di>
+      </>
     );
   }
 };
