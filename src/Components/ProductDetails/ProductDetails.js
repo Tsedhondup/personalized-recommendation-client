ae7deb11effefca0cd1b46f03470ec23a8f6ed45
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-02 18:58:23 -0400

    rendering of product detail completed

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,4 +1,84 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 const ProductDetails = () => {
-  return <h1>this is product detail page</h1>;
+  const [productLists, setProductLists] = useState([]);
+  const { productId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/currentData`)
+      .then((respond) => {
+        setProductLists(respond.data.parsedData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (productLists.length > 0) {
+    return (
+      <div className="product-detail">
+        <div className="product-detail__current-product">
+          {productLists.map((item) => {
+            if (item.product_id === productId) {
+              return (
+                <div className="current-product-item" key={item.product_id}>
+                  <div className="current-product-item__image">
+                    <img src={item.thumbnail} alt="product-image" />
+                  </div>
+                  <div className="current-product-item__info">
+                    <h1>{item.title}</h1>
+                    <h2>{`Brand: ${item.source}`}</h2>
+                    <h2>{`Rating: ${item.rating}`}</h2>
+                    <img src={item.source_icon} alt="brand-logo" />
+                    <h2>{item.price}</h2>
+                    <a
+                      href={item.product_link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Buy
+                    </a>
+                    <h2>{`Delivery: ${item.delivery}`}</h2>
+                    <h2>{item.reviews}</h2>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className="product-detail__next-products">
+          <div className="products">
+            {productLists.map((item) => {
+              return (
+                <Link
+                  to={`/${item.product_id}`}
+                  key={item.product_id}
+                  className=""
+                >
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    className="product-image"
+                  />
+                  <h1>{item.title}</h1>
+                  <h2>price: {item.price}</h2>
+                  <h2>
+                    {item.source}
+                    <img
+                      src={item.source_icon}
+                      alt="source-icon"
+                      className="source-icon"
+                    />
+                  </h2>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 export default ProductDetails;
