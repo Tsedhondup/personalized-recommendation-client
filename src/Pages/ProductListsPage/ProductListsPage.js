edef637e8c5fa5b040fb1e7300ff8e5762cbadc8
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-05 21:20:28 -0500

    URL decoding in productListPage working

--- src/Pages/ProductListsPage/ProductListsPage.js
+++ src/Pages/ProductListsPage/ProductListsPage.js
@@ -1,7 +1,17 @@
+import { useLocation } from "react-router-dom";
 const ProductListsPage = () => {
+  const { search } = useLocation(); // extract search part of the URL
+  const query = new URLSearchParams(search); // parses query string
+  /*
+   * fetches value associated with key "data"
+   * decode URL-encoded string,
+   * parse and converted back into Object
+   */
+  const data = JSON.parse(decodeURIComponent(query.get("data")));
   return (
     <div>
-      <h1>this is product list page</h1>
+      <h1>Product Lists Id: {data.productListsId}</h1>
+      <h1>Product Name: {data.productName}</h1>
     </div>
   );
 };
