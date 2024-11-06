7c7e498228996e2d562e7d371d5c6ed97efe9a3b
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-05 20:49:47 -0500

    event handlers created

--- src/Pages/SearchEnginePage/SearchEnginePage.js
+++ src/Pages/SearchEnginePage/SearchEnginePage.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
 const SearchEnginePage = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+  const handleSearchInput = (event) => {
+    setSearchInput(event.target.value);
+  };
+  const handleFormSubmit = () => {
+    const productListsObject = {
+      productListsId: uuidv4(),
+      productName: searchInput,
+    };
+  };
   return (
     <div>
-      <form>
-        <input placeholder="enter product name"></input>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <input
+          value={searchInput}
+          onChange={(event) => {
+            handleSearchInput(event);
+          }}
+          placeholder="enter product name"
+        ></input>
         <button>Search</button>
       </form>
     </div>
